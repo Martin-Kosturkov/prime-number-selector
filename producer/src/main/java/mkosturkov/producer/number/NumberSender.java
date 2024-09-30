6f8f4b5100f8e91022954eeedfa907d871e417ab
Martin Kosturkov <martin.kosturkovv@gmail.com>
2024-09-30 18:49:34 +0300

    Refactor number sender mark as @KafkaClient

--- producer/src/main/java/mkosturkov/producer/number/NumberSender.java
+++ producer/src/main/java/mkosturkov/producer/number/NumberSender.java
@@ -1,27 +1,12 @@
 package mkosturkov.producer.number;
 
 import io.micronaut.configuration.kafka.annotation.KafkaClient;
-import io.micronaut.context.annotation.Value;
-import jakarta.inject.Singleton;
+import io.micronaut.configuration.kafka.annotation.Topic;
 import mosturkov.common.NumberData;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
 
-@Singleton
-public class NumberSender {
+@KafkaClient
+public interface NumberSender {
 
-    private final Producer<?, NumberData> producer;
-    private final String topicName;
-
-    public NumberSender(
-            @KafkaClient Producer<?, NumberData> producer,
-            @Value("${numbers.kafka.topic-name}") String topicName) {
-
-        this.producer = producer;
-        this.topicName = topicName;
-    }
-
-    void send(NumberData generatedNumberData) {
-        producer.send(new ProducerRecord<>(topicName, generatedNumberData));
-    }
+    @Topic("${numbers.kafka.topic-name}")
+    void send(NumberData generatedNumberData);
 }
